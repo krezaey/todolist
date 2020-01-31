6b81b16bbd95be7d0bcfaedc60f1b30184dd74d7
kckc1289 <keziah.camille8@ymail.com>
2020-01-30 16:37:29 -0800

    Fixed little bug

--- src/AddTodo.js
+++ src/AddTodo.js
@@ -1,33 +1,32 @@
 import React, { Component } from 'react'
 
 class AddTodo extends Component {
-    state = {
-        content: ''
-    }
-
-    handleChange = (e) => {
-        this.setState({
-            content: e.target.value
-        })
-    }
-
-    handleSubmit = (e) => {
-        e.preventDefault();
-        this.props.addTodo(this.state);
-        this.setState({
-            content: ''
-        })
-    }
-
-    render() {
-        return (
-            <div>
-                <form onSubmit={this.handleSubmit}></form>
-                <label>Add new Todo: </label>
-                <input type="text" onChange={this.handleChange} value={this.state.content}/>
-            </div>
-        )
-    }
+  state = {
+    content: ''
+  }
+  handleChange = (e) => {
+    this.setState({
+      content: e.target.value
+    });
+  }
+  handleSubmit = (e) => {
+    e.preventDefault();
+    // call function to add a todo
+    this.props.addTodo(this.state);
+    this.setState({
+      content: ''
+    })
+  }
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label>Add a new todo:</label>
+          <input type="text" onChange={this.handleChange} value={this.state.content} />
+        </form>
+      </div>
+    )
+  }
 }
 
 export default AddTodo
--- src/App.js
+++ src/App.js
@@ -2,38 +2,34 @@ import React, { Component } from 'react';
 import Todos from './Todos'
 import AddTodo from './AddTodo'
 
-
-class App extends Component{
+class App extends Component {
   state = {
     todos: [
-      {id: 1, content: 'get money'},
-      {id: 2, content: 'money'}
+      {id: 1, content: 'buy some milk'},
+      {id: 2, content: 'play mario kart'}
     ]
   }
-
   deleteTodo = (id) => {
     const todos = this.state.todos.filter(todo => {
-      return todo.id !== id;
-    })
+      return todo.id !== id
+    });
     this.setState({
       todos
-    })
+    });
   }
-
   addTodo = (todo) => {
     todo.id = Math.random();
     let todos = [...this.state.todos, todo];
     this.setState({
       todos
-    })
+    });
   }
-
   render() {
     return (
-      <div className="App">
-      <h1 className="center blue-text">Todo's</h1>
-      <Todos todos={this.state.todos} deleteTodo={this.deleteTodo}/>
-      <AddTodo addTodo={this.addTodo}/>
+      <div className="todo-app container">
+        <h1 className="center blue-text">Todo's</h1>
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
+        <AddTodo addTodo={this.addTodo} />
       </div>
     );
   }
